4740b24081a577bac27c23553341a2434e59c07f
taufiqhida <taufiqh2003@gmail.com>
2023-01-30 14:50:15 +0700

    awal

--- app.py
+++ app.py
@@ -2,4 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Si Cantik Punya Si Ganteng')
+st.title('Ketuanya milik si cantik ferina')
+
+dv = pd.read_csv('Book1.csv')
+
+st.write(dv)
+
